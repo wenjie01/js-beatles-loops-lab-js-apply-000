2e0d80d3480eef4b38271da38af5468adf6e9719
Wenjie <xuwenjie8866@gmail.com>
2019-10-14 23:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var  array = [];
+  for (i = 0; i< musicians.length; i++){
+    var musician = musicians[i];
+    var instrument = instruments[i];
+    var newSentence = musician + 'plays' + instrument+'.';
+    array.push(newSentence)
+
+  }
+}
