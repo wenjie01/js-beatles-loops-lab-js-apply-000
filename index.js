c7e984d10d1af034b012fbf08941d6f7196f94b6
Wenjie <xuwenjie8866@gmail.com>
2019-10-14 23:18:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var  array = [];
-  for (i = 0; i< musicians.length; i++){
+  for (var i = 0; i< musicians.length; i++){
     var musician = musicians[i];
     var instrument = instruments[i];
     var newSentence = musician + 'plays' + instrument+'.';
